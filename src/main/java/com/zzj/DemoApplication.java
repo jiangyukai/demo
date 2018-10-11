7076f508cc383d368873908f17646c5d1585fbf4
jiangyukai <jiangyukai>
2018-10-11 14:31:16 +0800

    配置druid数据源监控

--- src/main/java/com/zzj/DemoApplication.java
+++ src/main/java/com/zzj/DemoApplication.java
@@ -1,14 +1,23 @@
 package com.zzj;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @MapperScan("com.zzj.mapper")//将项目中对应的mapper类的路径加进来就可以了,这个对应了项目中mapper（dao）所对应的包路径
 public class DemoApplication {
 
+    protected static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+
     public static void main(String[] args) {
+        logger.info("springboot启动***********************开始");
         SpringApplication.run(DemoApplication.class, args);
+        logger.info("springboot启动结束***********************");
     }
 }
